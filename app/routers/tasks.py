6542fe10801b2771125fa583158b4676ec6a0b8a
Marek Zelinka <mzelinka17@gmail.com>
2026-01-15 16:51:36 +0100

    refactor filtering

--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -1,14 +1,18 @@
 import uuid
 from collections.abc import Sequence
 from datetime import UTC, datetime, time
-from enum import StrEnum, auto
 from typing import Annotated
 
-from fastapi import APIRouter, Body, Depends, HTTPException, Path, Query, status
+from fastapi import APIRouter, Body, HTTPException, Path, Query, status
 from sqlmodel import col, select
 
 from app.deps import CurrentUserDep, SessionDep
-from app.models import Task, TaskCreate, TaskPublic, TaskUpdate
+from app.models import (
+    Task,
+    TaskCreate,
+    TaskPublic,
+    TaskUpdate,
+)
 
 router = APIRouter(prefix="/tasks", tags=["tasks"])
 
@@ -27,29 +31,6 @@ async def create_task(
     return db_task
 
 
-class TaskView(StrEnum):
-    TODAY = auto()
-    UPCOMMING = auto()
-    OVERDUE = auto()
-
-
-def validate_task_filters(
-    completed: Annotated[
-        bool | None, Query(description="Filter by status. Exclusive with `view`.")
-    ] = None,
-    view: Annotated[
-        TaskView | None,
-        Query(description="Filter by time prespective. Exclusive with `completed`."),
-    ] = None,
-) -> tuple[bool | None, TaskView | None]:
-    if completed is not None and view is not None:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Parameters 'completed' and 'view' are mutually exclusive, provide only one or the other",
-        )
-    return completed, view
-
-
 @router.get("/", response_model=list[TaskPublic])
 async def read_tasks(
     *,
@@ -57,32 +38,72 @@ async def read_tasks(
     current_user: CurrentUserDep,
     offset: Annotated[int, Query(ge=0)] = 0,
     limit: Annotated[int, Query(gt=0)] = 100,
-    filters: Annotated[
-        tuple[bool | None, TaskView | None], Depends(validate_task_filters)
-    ],
+    completed: Annotated[bool | None, Query()] = None,
 ) -> Sequence[Task]:
     query = select(Task).where(Task.owner_id == current_user.id)
-    completed, view = filters
-    now = datetime.now(UTC)
-    # Apply view-specific filters
-    if view == TaskView.TODAY:
-        today_end = datetime.combine(now.date(), time.max, tzinfo=UTC)
-        today_start = datetime.combine(now.date(), time.min, tzinfo=UTC)
-        query = query.where(col(Task.due_date).between(today_start, today_end))
-    elif view == TaskView.UPCOMMING:
-        query = query.where(col(Task.due_date) > now)
-    elif view == TaskView.OVERDUE:
-        query = query.where(col(Task.due_date) < now)
-    # Apply task completion defaults
     if completed is not None:
-        # Respect the explicit user fiter
         query = query.where(Task.completed == completed)
-    elif view is not None:
-        # Default behavior for prespectives when COMPLETED is not explicitly set
-        query = query.where(col(Task.completed).is_(False))
-    # Apply pagination and ordering
+    results = await session.exec(query.offset(offset).limit(limit))
+    return results.all()
+
+
+@router.get("/upcomming", response_model=list[TaskPublic])
+async def read_upcomming_tasks(
+    *,
+    session: SessionDep,
+    current_user: CurrentUserDep,
+    offset: Annotated[int, Query(ge=0)] = 0,
+    limit: Annotated[int, Query(gt=0)] = 100,
+) -> Sequence[Task]:
+    now = datetime.now(UTC)
     results = await session.exec(
-        query.order_by(col(Task.due_date).asc().nulls_last())
+        select(Task)
+        .where(Task.owner_id == current_user.id)
+        .where(col(Task.due_date) > now)
+        .where(col(Task.completed).is_(False))
+        .order_by(col(Task.due_date).asc().nulls_last())
+        .offset(offset)
+        .limit(limit)
+    )
+    return results.all()
+
+
+@router.get("/today", response_model=list[TaskPublic])
+async def read_due_today_tasks(
+    *,
+    session: SessionDep,
+    current_user: CurrentUserDep,
+    offset: Annotated[int, Query(ge=0)] = 0,
+    limit: Annotated[int, Query(gt=0)] = 100,
+) -> Sequence[Task]:
+    now = datetime.now(UTC)
+    today_end = datetime.combine(now.date(), time.max, tzinfo=UTC)
+    today_start = datetime.combine(now.date(), time.min, tzinfo=UTC)
+    results = await session.exec(
+        select(Task)
+        .where(Task.owner_id == current_user.id)
+        .where(col(Task.due_date).between(today_start, today_end))
+        .where(col(Task.completed).is_(False))
+        .offset(offset)
+        .limit(limit)
+    )
+    return results.all()
+
+
+@router.get("/overdue", response_model=list[TaskPublic])
+async def read_overdue_tasks(
+    *,
+    session: SessionDep,
+    current_user: CurrentUserDep,
+    offset: Annotated[int, Query(ge=0)] = 0,
+    limit: Annotated[int, Query(gt=0)] = 100,
+) -> Sequence[Task]:
+    now = datetime.now(UTC)
+    results = await session.exec(
+        select(Task)
+        .where(Task.owner_id == current_user.id)
+        .where(col(Task.due_date) < now)
+        .where(col(Task.completed).is_(False))
         .offset(offset)
         .limit(limit)
     )
